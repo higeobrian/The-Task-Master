090605fd310ef86b188bc7cee3f96b636ba2072b
Brian Seward <blacktiger55@hotmail.com>
2018-11-06 23:53:53 -0700

    Set up server side file structure

---
+++ server/models/board.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema 
+var ObjectId = Schema.Types.ObjectId
+var schemaName = 'Board'
+
+
+var boardSchema = new Schema ({
+    title: {type: String, required: true},
+    body: {type: String, required: true},
+    userId: {type: ObjectId, ref: 'User', required: true }
+})
+
+
+module.exports = mongoose.model(schemaName, boardSchema)
