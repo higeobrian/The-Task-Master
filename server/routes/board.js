4f1eeaf54fa794408d54bc6ecff7b81e8808aa94
Brian Seward <blacktiger55@hotmail.com>
2018-11-06 23:58:38 -0700

    routes completed

---
+++ server/routes/board.js
@@ -0,0 +1,66 @@
+let router = require('express').Router()
+var Board = require('../models/board')
+var User = require('../models/user')
+
+//GET BY ID
+router.get('/api/boards/:id', (req, res, next)=>{
+    Board.findById(req.params.id)
+      .then(board =>{
+        res.status(200).send(board)
+      })
+      .catch(err => {
+        res.status(400).send(err)
+      })
+  })
+
+  //Get Users Boards  VUE REQUEST api.get('/boards/')
+  router.get('/api/boards/user/:id', (req, res, next)=>{
+    Board.find({userId: req.session.uid})
+    .then(boards=>{
+        res.send(boards)
+    })
+    .catch(err=>{
+        res.status(500).send(err)
+    })
+  })
+  
+
+  //ADD
+  router.post('/api/boards', (req, res, next) => {
+    var board = req.body
+    console.log(board)
+    board.userId = req.session.uid
+    Board.create(board)
+      .then(newBoard => {
+        res.status(200).send(newBoard)
+      })
+      .catch(err => {
+        res.status(400).send(err)
+      })
+  })
+  
+  //EDIT
+  router.put('/api/boards/:id', (req, res, next) => {
+    Board.findByIdAndUpdate(req.params.id, req.body, {new: true})
+      .then(board => {
+        res.status(200).send({message: "Successfully Updated", board})
+      })
+      .catch(err => {
+        res.status(400).send(err)
+      })
+  })
+  
+  //DESTROY
+  router.delete('/api/boards/:id', (req, res, next)=>{
+    Board.findByIdAndRemove(req.params.id)
+      .then(data=>{
+        res.send("Successfully Deleted board")
+      })
+      .catch(err => {
+        res.status(400).send(err)
+      })
+  })
+
+module.exports = {
+    router
+  }
---
+++ server/routes/list.js
@@ -0,0 +1,70 @@
+var router = require('express').Router()
+var List = require('../models/list')
+
+//GET BY ID
+router.get('/api/lists/:id', (req, res, next)=>{
+    List.findById(req.params.id)
+      .then(list =>{
+        res.status(200).send(list)
+      })
+      .catch(err => {
+        res.status(400).send(err)
+      })
+  })
+  
+  router.get('/api/boards/:id/lists', (req, res, next)=>{
+    
+    List.find({boardId: req.params.id})
+    .then(boards=>{
+      console.log(boards)
+        res.send(boards)
+    })
+    .catch(err=>{
+        res.status(500).send(err)
+    })
+  })
+
+// "userId": "5b0dc102a6a35b187c3e6091", 
+// "boardId": "5b0dc2bbe29d2544b4c7e98f"
+
+  //ADD
+  router.post('/api/lists', (req, res, next) => {
+    var list = req.body
+    console.log(list)
+    console.log(req)
+    list.userId = req.session.uid
+    // list.boardId = 
+    List.create(list)
+      .then(newList => {
+        res.status(200).send(newList)
+      })
+      .catch(err => {
+        res.status(400).send(err)
+      })
+  })
+  
+  //EDIT
+  router.put('/api/lists/:id', (req, res, next) => {
+    List.findByIdAndUpdate(req.params.id, req.body, {new: true})
+      .then(list => {
+        res.status(200).send({message: "Successfully Updated", list})
+      })
+      .catch(err => {
+        res.status(400).send(err)
+      })
+  })
+  
+  //DESTROY
+  router.delete('/api/lists/:id', (req, res, next)=>{
+    List.findByIdAndRemove(req.params.id)
+      .then(data=>{
+        res.send("Successfully Deleted list")
+      })
+      .catch(err => {
+        res.status(400).send(err)
+      })
+  })
+
+module.exports = {
+    router
+  }
