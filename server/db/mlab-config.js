ab16f5168fd7ae6baee10b4c6a5808a0cce8d203
Brian Seward <blacktiger55@hotmail.com>
2018-11-08 00:00:31 -0700

    db set up

---
+++ server/db/mlab-config.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose')
+var connectionString = 'mongodb://<dbuser>:<dbpassword>@ds155313.mlab.com:55313/briantaskmanager' //Jasper Server
+var connection = mongoose.connection
+
+
+mongoose.connect(connectionString)
+
+connection.on('error', err=>{
+  console.log('ERROR FROM DATABASE: ', err)
+})
+
+
+connection.once('open', ()=>{
+  console.log('Connected to Database')
+})
